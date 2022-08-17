249c1ee49c438a2fa6fe2504a662bfa32670fe8c
Tianling Shen <i@cnsztl.eu.org>
2022-08-17 23:27:23 +0800

    chore: use Loyalsoldier's geoip
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- main.go
+++ main.go
@@ -189,7 +189,7 @@ func release(source string, destination string) error {
 	if err != nil {
 		logrus.Warn("missing destination latest release")
 	} else {
-		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.Name) {
+		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.TagName) {
 			logrus.Info("already latest")
 			setActionOutput("skip", "true")
 			return nil
@@ -226,7 +226,7 @@ func release(source string, destination string) error {
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
+	setActionOutput("tag", *sourceRelease.TagName)
 	return nil
 }
 
@@ -239,7 +239,7 @@ func main() {
 	if len(os.Args) >= 3 {
 		err = local(os.Args[1], os.Args[2], os.Args[2:])
 	} else {
-		err = release("Dreamacro/maxmind-geoip", "sagernet/sing-geoip")
+		err = release("Loyalsoldier/geoip", "1715173329/sing-geoip")
 	}
 	if err != nil {
 		logrus.Fatal(err)
